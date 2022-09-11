7dec59d4cfe80680a4f52f825691cfe69c563479
Zaid <karaymehzaid@gmail.com>
2022-09-11 15:40:45 +0300

    prod

--- backend/settings.py
+++ backend/settings.py
@@ -11,17 +11,21 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i=l-jtxx##acx*$&p%p94v57)3yhqeb7b3#tkfp3(1p!0f25up'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
