c758c0d73c804ccb7973931fd184f60a305c7e31
Zaid <karaymehzaid@gmail.com>
2022-09-11 16:44:44 +0300

    Prod Config

--- backend/settings.py
+++ backend/settings.py
@@ -87,6 +87,14 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+DATABASES = {
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
